bd6b04bde2ae1d7c730c9133e9d408c92c321975
JelenaPenkina <122533981+JelenaPenkina@users.noreply.github.com>
2025-11-16 21:58:27 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using NUnit.Framework.Internal;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 
@@ -9,53 +8,54 @@ internal class Program
     {
         TestKindergartenAdd();
     }
+
     [Test]
     public static void TestKindergartenAdd()
     {
-        IWebDriver driver = new FirefoxDriver();  //uus fiorefoxi driver
-        driver.Url = "https://localhost:7106";   // navigeerib siia 
+        IWebDriver driver = new FirefoxDriver();  // uus firefox driver
+        driver.Url = "https://localhost:7106";    // navigeerib siia 
 
-        IWebElement idOfLinkElement = driver.FindElement(By.Id("kindergarten")); //otsi element spaceship id-ga
-        idOfLinkElement.Click(); //vajuta sellele elemendile
+        // NAVIGEERIMINE KG lehele ning ka otsing ID järgi 
+        IWebElement idOfLinkElement = driver.FindElement(By.Id("kindergarten"));
+        idOfLinkElement.Click();
         Thread.Sleep(500);
 
+        // LISA UUS KINDERGARTEN
         IWebElement idOfCreateButton = driver.FindElement(By.Id("addBtn"));
         idOfCreateButton.Click();
         Thread.Sleep(500);
 
-        InsertSpaceShipData(driver);
+        InsertKindergartenData(driver);
 
         IWebElement idOfCreatePostButton = driver.FindElement(By.Id("createBtn"));
         idOfCreatePostButton.Click();
         Thread.Sleep(500);
 
-        ICollection<IWebElement> elementsToCheck = driver.FindElements(By.Id("testIdNAM_I"));
-   
-
-        IWebElement idOfTestData1 = driver.FindElement(By.Id("testIdNAM_I"));
+        // KONTROLL INDEX VAATES
+        IWebElement idOfTestData1 = driver.FindElement(By.Id("kgIndexName"));
         var nameInIndex = idOfTestData1.Text;
-        IWebElement idOfTestData2 = driver.FindElement(By.Id("testIdEPW_I"));
-        var powerInIndex = idOfTestData2.Text;
 
-        Assert.That(nameInIndex, Is.EqualTo("TESTNAME_01"));
-        Assert.That(powerInIndex, Is.EqualTo("24857"));
+        IWebElement idOfTestData2 = driver.FindElement(By.Id("kgIndexTeacher"));
+        var teacherInIndex = idOfTestData2.Text;
+
+        Assert.That(nameInIndex, Is.EqualTo("Sunny"));
+        Assert.That(teacherInIndex, Is.EqualTo("Teacher Tauno"));
+
         Console.WriteLine("KG Test passed");
-
     }
-    private static void InsertSpaceShipData(IWebDriver driver)
-    {
 
+    private static void InsertKindergartenData(IWebDriver driver)
+    {
         IWebElement name = driver.FindElement(By.Id("KindergartenName"));
         name.SendKeys("Sunny");
 
         IWebElement group = driver.FindElement(By.Id("GroupName"));
         group.SendKeys("Milka");
 
-        IWebElement teacher = driver.FindElement(By.Id("Teacher"));
+        IWebElement teacher = driver.FindElement(By.Id("TeacherName"));
         teacher.SendKeys("Teacher Tauno");
 
-        IWebElement children = driver.FindElement(By.Id("Children"));
+        IWebElement children = driver.FindElement(By.Id("ChildrenCount"));
         children.SendKeys("24");
     }
 }
-
